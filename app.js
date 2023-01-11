ee1a4bbfda5f82861aa479baacc445d81d8982aa
Humbertov374 <humbertov374@gmail.com>
2023-01-10 17:46:26 -0700

    builder function completed

--- app.js
+++ app.js
@@ -4,8 +4,130 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
+const teamProfile = [];
+function teamProfileBuilder() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Hello Team Manager, please enter your name"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter your ID."
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter your Email."
+            },
+            {
+                type: "input",
+                name: "officeNumber",
+                message: "Please enter your office number."
+            }
+        ])
+        .then(data => {
+            let manager = new Manager(data.name, data.id, data.email, data.officeNumber);
+
+            teamProfile.push(manager);
+            addMember();
+        })
+}
+
+function addMember() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "member",
+                message: "Select another team member to add, or complete team.",
+                choices: ["Engineer", "Intern", "Team Complete"]
+            }
+        ])
+}
+        .then(choice => {
+            if (choice.member === "Engineer") {
+                addEngineer();
+            }
+            else if (choice.member === "Intern") {
+                addIntern();
+            }
+            else {
+                completeProfile();
+            }
+        })
+}
+
+function completeProfile() {
+    console.dir(teamProfile);
+}
+
+function addEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter Engineer's name"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter Engineer's ID."
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter Engineer's Email."
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "Please enter Engineer's github username."
+            }
+        ])
+        .then(data => {
+            let engineer = new Engineer(data.name, data.id, data.email, data.github);
+            teamProfile.push(engineer);
+            addMember();
+        })
+}
+
+function addIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Please enter Intern's name"
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "Please enter Intern's ID."
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "Please enter Intern's Email."
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "Please enter Intern's School name."
+            }
+        ])
+        .then(data => {
+            let intern = new Intern(data.name, data.id, data.email, data.school);
+            teamProfile.push(intern);
+            addMember();
+        })
+}
+
+teamProfileBuilder();
